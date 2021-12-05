dc3318b66b39823c00db80a1270436a93cd2659b
Eduardo Felipe <edu_felip@hotmail.com>
2021-12-05 09:28:03 -0300

    project refactor

--- mymalloc.c
+++ mymalloc.c
@@ -1,102 +1,152 @@
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
 
-void *global_pointer = NULL;
-
-typedef struct chunk Chunk;
 struct chunk {
     size_t size;
     struct chunk *next;
+    struct chunk *prev;
+    void *pointer;
     int free;
-};
+    char data[1];
+} __attribute__((packed));
 
-#define CHUNK_SIZE sizeof(Chunk)
+#define BLOCK_SIZE (sizeof(struct chunk) - 1)
 
-int MyMallocInit(int size) {
-    void *request = sbrk(size);
-    if(request == (void*) -1)
-        return 0;
-    return 1;
-}
+typedef struct chunk *block;
 
-Chunk *find_free_chunk(Chunk** last, size_t size) {
-    Chunk *current = global_pointer;
-    while(current && !(current->free && current->size >= size)) {
-        *last = current;
-        current = current->next;
-    }
-    return current;
-}
+void *global_pointer = NULL;
 
-Chunk *request_space(Chunk* last, size_t size) {
-    Chunk* block;
-    block = sbrk(0);
-    void *request = sbrk(size + CHUNK_SIZE);
-    assert((void*) block == request);
-    if(request == (void*) - 1) {
-        return NULL;
-    }
-    if(last) {
-        last->next = block;
-    }
-    block->size = size;
-    block->next = NULL;
-    block->free = 0;
-    return block;
-}
-
-void split_chunk(Chunk *block, size_t size) {
-    Chunk *new;
-    new->size = block->size - CHUNK_SIZE - size;
-    new->next = block->next;
+void split_block(block b, size_t s) {
+    block new;
+    new = (block)(b->data + s);
+    new->size = b->size - s - BLOCK_SIZE;
+    new->next = b->next;
+    new->prev = b;
     new->free = 1;
-    block->size = size;
-    block->next = new;
+    new->pointer = new->data;
+    b->size = s;
+    b->next = new;
+    if(new->next)
+        new->next->prev = new;
 }
 
-void *allocam(size_t size) {
-    Chunk *block;
-    if(size <= 0) 
-        return NULL;
-    if(!global_pointer) {
-        block = request_space(NULL, size);
-        if(!block)
-            return NULL;
+block find_block(block *last, size_t size) {
+    block b = (block) global_pointer;
+    while(b && !(b->free && b->size >= size)) {
+        *last = b;
+        b = b->next;
     }
-    else {
-        Chunk *last = global_pointer;
-        block = find_free_chunk(&last, size);
-        if(!block) {
-            block = request_space(last, size);
-            if(!block)
-                return NULL;
+    return b;
+}
+
+block extend_heap(block last, size_t s) {
+    block b = (block) sbrk(0);
+    if(sbrk(BLOCK_SIZE + s) == (void *) -1) {
+        return NULL;
+    }
+    b->size = s;
+    b->next = NULL;
+    b->prev = last;
+    b->pointer = b->data;
+    if(last) {
+        last->next = b;
+    } 
+    b->free = 0;
+    return b;
+}
+
+void *malloc(size_t size) {
+    block b;
+    if(global_pointer) {
+        block last = (block) global_pointer;
+        b = find_block(&last, size);
+        if(b) {
+            if((b->size - size) >= (BLOCK_SIZE))
+                split_block(b, size);
+            b->free = 0;
         } else {
-            if (size < block->size) split_chunk(block, size);
-            block->free = 0;
+            b = extend_heap(last, size);
+            if(!b)
+                return NULL;
+        }
+    } else {
+        b = extend_heap(NULL, size);
+        if(!b)
+            return NULL;
+        global_pointer = b;
+    }
+    return b->data;
+}
+
+block get_block(void *p) {
+    char *tmp;
+    tmp = p;
+    return (p = tmp -= BLOCK_SIZE);
+}
+
+int valid_addr(void *p) {
+    if(global_pointer) {
+        if(p > global_pointer && p < sbrk(0)) {
+            block test = get_block(p);
+            return p == (get_block(p)->pointer);
         }
     }
-    return(block + 1); // nós queremos retornar o ponteiro para a regiao após o metadata do bloco
+    return 0;
 }
 
-Chunk *get_chunk_pointer(void *pointer) {
-    return (Chunk*) pointer - 1;
+block fusion(block b) {
+    if(b->next && b->next->free) {
+        b->size += BLOCK_SIZE + b->next->size;
+        b->next = b->next->next;
+        if(b->next)
+            b->next->prev = b;
+    }
+    return b;
 }
 
-void freem(void *pointer) {
-    if(!pointer)
-        return;
-    Chunk *chunk_pointer = get_chunk_pointer(pointer);
-    assert(chunk_pointer->free == 0);
-    chunk_pointer->free = 1;
+void free(void *p) {
+    if (valid_addr(p)) {
+        block b = get_block(p);
+        b->free = 1;
+        if(b->prev && b->prev->free) {
+            b = fusion(b->prev);
+        }
+        if(b->next) {
+            fusion(b);
+        } else {
+            if(b->prev) {
+                b->prev->next = NULL;
+            } else {
+                global_pointer = NULL;
+            }
+            brk(b);
+        }
+    }
+}
+
+void MyMallocGerency() {
+    printf("------MEMORY SPACES-----\n");
+    block b = global_pointer;
+    while(b && b->next) {
+        printf("%p", (void *) b);
+        printf(" - ");
+        printf("TAMANHO: %ld BYTES", b->size);
+        printf(" - ");
+        if(b->free) {
+            printf("LIVRE\n");
+        } else {
+            printf("OCUPADO\n");
+        }
+        b = b->next;
+    }
 }
 
 int main(void) {
-    for(int i = 0; i < 10; i++) {
-        void *p = allocam(i);
-        printf("%p\n", (void *) p);
-    }
+    int *a = malloc(4* sizeof(int));
+    int *b = malloc(4* sizeof(int));
+    free(a);
+    int *c = malloc(sizeof(int));
+    MyMallocGerency();
     return 0;
 }
