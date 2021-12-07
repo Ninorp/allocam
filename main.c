9b6528af94c383d4a838957a8e6079dbcae70063
Eduardo Felipe <edu_felip@hotmail.com>
2021-12-07 11:39:46 -0300

    add choice option

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 
-#include <time.h>
-#include <assert.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 #include "lib/mymalloc.h"
 #include "test/test.h"
 
 int main(void) {
-    test_time_malloc();
-    test_time_mymalloc();
-    test_performance_malloc();
-    test_performance_mymalloc();
-    test_fragmentation_mymalloc();
-
+    int choice;
+    printf("1 - Testar Tempo | 2 - Testar Performance | 3 - Testar Fragmentação (MyMalloc) \n");
+    scanf("%d", &choice);
+    switch(choice) {
+        case 1:
+            test_time_malloc();
+            test_time_mymalloc();
+            break;
+        case 2:
+            test_performance_malloc();
+            test_performance_mymalloc();
+            break;
+        case 3:
+            test_fragmentation_mymalloc();
+            break;
+    }
     return 0;
 }
